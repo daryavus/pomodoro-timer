0e1d5babc4b7fac717e9e35c7f637a51ccefb06f
daryavus <vus.dasha@gmail.com>
2025-04-25 14:47:31 +0300

    created a script for the timer

---
+++ script.js
@@ -0,0 +1,86 @@
+const btnWork = document.querySelector('#pomodoro');
+const btnBreak = document.querySelector('#break');
+const btnStart = document.querySelector('#start');
+const btnReset = document.querySelector('#reset');
+const timer = document.querySelector('#pomodoro-time');
+
+let totalSeconds = 1500;
+let timerId = null;
+
+function updateTimerDisplay() {
+  const minutes = Math.floor(totalSeconds / 60);
+  const seconds = totalSeconds % 60;
+
+  const formattedTime = `${String(minutes).padStart(2, '0')}:${String(seconds).padStart(2, '0')}`;
+  timer.textContent = formattedTime;
+}
+
+btnWork.addEventListener('click', function() {
+  totalSeconds = 1500;
+  updateTimerDisplay();
+
+  btnWork.classList.add('active');
+  btnBreak.classList.remove('active');
+
+  if(timerId) {
+    clearInterval(timerId);
+    timerId = null;
+    btnStart.textContent = 'Start';
+  }
+});
+
+btnBreak.addEventListener('click', function() {
+  totalSeconds = 300;
+  updateTimerDisplay();
+
+  btnBreak.classList.add('active');
+  btnWork.classList.remove('active');
+
+  if(timerId) {
+    clearInterval(timerId);
+    timerId = null;
+    btnStart.textContent = 'Start';
+  }
+});
+
+btnStart.addEventListener('click', function() {
+  if(timerId) {
+    clearInterval(timerId);
+    timerId = null;
+    btnStart.textContent = 'Start';
+  } else {
+    btnStart.textContent = 'Stop';
+    timerId = setInterval(() => {
+      if (totalSeconds > 0) {
+        totalSeconds--;
+        updateTimerDisplay();
+      } else {
+        clearInterval(timerId);
+        timerId = null;
+        if(btnWork.classList.contains('active')) {
+          totalSeconds = 1500;
+        } else {
+          totalSeconds = 300;
+        }
+        updateTimerDisplay();
+        btnStart.textContent = 'Start';
+      }
+    }, 1000);
+  }
+});
+
+btnReset.addEventListener('click', function() {
+  clearInterval(timerId);
+  timerId = null;
+
+  if(btnWork.classList.contains('active')) {
+    totalSeconds = 1500;
+  } else {
+    totalSeconds = 300;
+  }
+
+  updateTimerDisplay();
+  btnStart.textContent = 'Start';
+});
+
+
